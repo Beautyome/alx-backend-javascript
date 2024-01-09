5888ed05531b7fa44ea67edc74d7c150c4604d71
Beautyome <beautyupeleomo@gmail.com>
2024-01-09 15:40:38 -0800

    Update

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || !Array.from(set).every(el => typeof el === 'string')) return '';
-
+  if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
   return [...set]
     .filter((el) => el.startsWith(startString))
     .map((el) => el.slice(startString.length))
